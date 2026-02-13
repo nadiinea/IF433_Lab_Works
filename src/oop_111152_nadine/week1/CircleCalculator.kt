cc9a62468ebd1e9785761fddaf3e2679f0e14404
nadiinea <naadiinea@gmail.com>
2026-02-13 22:14:36 +0700

    week01: circle calculator

---
+++ src/oop_111152_nadine/week1/CircleCalculator.kt
@@ -0,0 +1,13 @@
+package oop_111152_nadine.week1
+
+fun main() {
+    val radius = 7.0
+    val pi = 3.14
+    val area = pi * radius * radius
+
+    println("Radius: $radius, Area: $area")
+    println(checkSize(area))
+}
+
+fun checkSize(area: Double) =
+    if (area > 100) "This is a Big Circle" else "This is a Small Circle"
