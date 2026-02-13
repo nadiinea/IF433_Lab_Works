c7b23a25a3e7774bb49111082a2f68ec57e921af
nadiinea <naadiinea@gmail.com>
2026-02-13 12:29:38 +0700

    week02: (task 1) create hero Class

--- src/oop_111152_nadine/week2/Main.kt
+++ src/oop_111152_nadine/week2/Main.kt
@@ -8,6 +8,34 @@ fun main() {
 
     val scanner = Scanner(System.`in`)
 
+    println("=== APLIKASI PMB UMN ===")
+
+    print("Masukkan Nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan NIM (Wajib 5 Karakter): ")
+    val nim = scanner.nextLine()
+
+    if (nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+    } else {
+        print("Pilih Jalur (1. Reguler, 2. Umum): ")
+        val type = scanner.nextInt()
+        scanner.nextLine()
+
+        if (type == 1) {
+            print("Masukkan Jurusan: ")
+            val major = scanner.nextLine()
+            val s1 = Student(name, nim, major)
+            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+        } else if (type == 2) {
+            val s2 = Student(name, nim)
+            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+        } else {
+            println("Pilihan ngawur, pendaftaran batal!")
+        }
+    }
+
     println("=== LIBRARY FINE SYSTEM ===")
 
     print("Judul Buku: ")
