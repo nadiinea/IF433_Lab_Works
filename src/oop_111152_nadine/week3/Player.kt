eb517aa1c7ae3e8b2b961be2ec683b62d6bea30a
nadiinea <naadiinea@gmail.com>
2026-02-20 21:23:35 +0700

    week3: (task 2) create Player class with priv xp

---
+++ src/oop_111152_nadine/week3/Player.kt
@@ -0,0 +1,24 @@
+package oop_111152_nadine.week3
+
+class Player(val username: String) {
+
+    private var xp: Int = 0
+
+    val level: Int
+        get() = (xp / 100) + 1
+
+    fun addXp(amount: Int) {
+        if (amount <= 0) {
+            println("XP harus bernilai positif!")
+            return
+        }
+
+        val oldLevel = level
+        xp += amount
+        val newLevel = level
+
+        if (newLevel > oldLevel) {
+            println("Level Up! Selamat $username naik ke level $newLevel")
+        }
+    }
+}
