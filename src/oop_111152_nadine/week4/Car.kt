b86a1256f3896f0e2825b12b4bd6b9936b2056a3
nadiinea <naadiinea@gmail.com>
2026-02-27 10:41:14 +0700

    week4: implement super keyword in Car accelerate

--- src/oop_111152_nadine/week4/Car.kt
+++ src/oop_111152_nadine/week4/Car.kt
@@ -7,4 +7,9 @@ open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
     override fun honk() {
         println("TIN TIN! Mobil $brand lewat!")
     }
+
+    override fun accelerate() {
+        super.accelerate()
+        println("Mobil $brand menggunakan transmisi gigi utk menambah kecepatan")
+    }
 }
