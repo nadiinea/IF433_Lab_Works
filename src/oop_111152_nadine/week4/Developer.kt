a5818a457e783490b38f85ef536076bbf834cfda
nadiinea <naadiinea@gmail.com>
2026-02-27 11:18:54 +0700

    week4: (task 2) create Developer class

---
+++ src/oop_111152_nadine/week4/Developer.kt
@@ -0,0 +1,12 @@
+package oop_111152_nadine.week4
+
+class Developer(
+    name: String,
+    baseSalary: Int,
+    val programmingLanguage: String
+) : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang ngoding menggunakan $programmingLanguage.")
+    }
+}
