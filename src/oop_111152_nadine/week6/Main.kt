2f1dbe3a9946edde1d0a557deb92aed1500849f3
nadiinea <naadiinea@gmail.com>
2026-03-13 21:05:36 +0700

    week6: (task) instantiate smart devices in main

--- src/oop_111152_nadine/week6/Main.kt
+++ src/oop_111152_nadine/week6/Main.kt
@@ -1,21 +1,9 @@
 package oop_111152_nadine.week6
 
-fun processCheckout(method: PaymentMethod, amount: Double) {
-    println("-> Memulai checkout..")
-    method.pay(amount)
-}
-
 fun main() {
-    val myWatch = Smartwatch()
-    myWatch.showTime()
 
-    val myPhone = Smartphone()
-    myPhone.turnOn()
+    val lamp = SmartLamp("L1", "Ruang Tamu")
+    val speaker = SmartSpeaker("S1", "Google Nest Dapur")
+    val cctv = SmartCCTV("C1", "Ezviz Garasi")
 
-    val pay1 = Gopay()
-    val pay2() = CreditCard()
-
-    println("\n--- TETING CHECKOUT ---")
-    processCheckout(pay1, 50000)
-    processCheckout(pay2, 150000)
 }
