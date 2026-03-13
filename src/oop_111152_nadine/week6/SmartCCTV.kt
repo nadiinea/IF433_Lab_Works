88c6a4887f376e0b526d0509d27f46fce0417633
nadiinea <naadiinea@gmail.com>
2026-03-13 21:02:19 +0700

    week6: (task) create SmartCCTV with compound interfaces

---
+++ src/oop_111152_nadine/week6/SmartCCTV.kt
@@ -0,0 +1,20 @@
+package oop_111152_nadine.week6
+
+class SmartCCTV(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable, Recordable {
+
+    override fun turnOn() {
+        println("CCTV $name dinyalakan.")
+        startRecord()
+    }
+
+    override fun turnOff() {
+        println("CCTV $name dimatikan.")
+    }
+
+    override fun startRecord() {
+        println("CCTV $name mulai merekam.")
+    }
+}
