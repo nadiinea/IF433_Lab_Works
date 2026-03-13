53d9c8a8da7cf669bd9ccbc266d5761abe4b1fe8
nadiinea <naadiinea@gmail.com>
2026-03-13 21:19:13 +0700

    week6: (task) test smart home system logic in main

--- src/oop_111152_nadine/week6/Main.kt
+++ src/oop_111152_nadine/week6/Main.kt
@@ -1,6 +1,6 @@
 package oop_111152_nadine.week6
 
-fun main()
+fun main() {
     val lamp = SmartLamp("L1", "Ruang Tamu")
     val speaker = SmartSpeaker("S1", "Google Nest Dapur")
     val cctv = SmartCCTV("C1", "Ezviz Garasi")
