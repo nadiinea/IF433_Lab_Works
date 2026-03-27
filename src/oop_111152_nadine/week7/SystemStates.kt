734c380e7671eaaa6443cfc76617c7d57a5119f5
nadiinea <naadiinea@gmail.com>
2026-03-27 18:12:41 +0700

    week7: create AppState enum

---
+++ src/oop_111152_nadine/week7/SystemStates.kt
@@ -0,0 +1,5 @@
+package oop_111152_nadine.week7
+
+enum class AppState {
+    STARTING, RUNNING, STOPPED
+}
---
+++ src/oop_111152_nadine/week7/UserModels.kt
@@ -0,0 +1,5 @@
+package oop_111152_nadine.week7
+
+class RegularUser(val name: String, val age: Int)
+
+data class DataUser(val name: String, val age: Int)
