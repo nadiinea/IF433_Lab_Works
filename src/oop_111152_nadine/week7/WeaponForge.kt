60333f26af10adc1cc557eb82346997991254210
nadiinea <naadiinea@gmail.com>
2026-03-27 18:34:13 +0700

    week7: (task) implement factory pattern in WeaponForge

---
+++ src/oop_111152_nadine/week7/WeaponForge.kt
@@ -0,0 +1,18 @@
+package oop_111152_nadine.week7
+
+class Weapon private constructor(
+    val item: GameItem,
+    val durability: Int
+) {
+    companion object {
+        fun forgeStarterSword(): Weapon {
+            val item = GameItem("Pedang Kayu Bapuk", 5, ItemRarity.COMMON)
+            return Weapon(item, 50)
+        }
+
+        fun forgeEpicSword(): Weapon {
+            val item = GameItem("Pedang Naga", 100, ItemRarity.EPIC)
+            return Weapon(item, 200)
+        }
+    }
+}
