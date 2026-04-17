02dcb25fb5c31ca549a616a11fb16518843c3d13
nadiinea <naadiinea@gmail.com>
2026-04-17 23:19:03 +0700

    week08: implement chained safe calls and elvis

---
+++ src/oop_111152_nadine/week08/Main.kt
@@ -0,0 +1,9 @@
+package oop_111152_nadine.week08
+
+fun main() {
+    println("=== TEST SAFE CALLS & ELVIS ===")
+    val emptyOrder = Order(null, null)
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name
+        ?: "Kota Tidak Diketahui"
+    println("Tujuan pengiriman: $destination")
+}
