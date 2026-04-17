f2d9f2ee9111a36f1897d0aaeb53c1b4d9dd448e
nadiinea <naadiinea@gmail.com>
2026-04-17 23:18:07 +0700

    week08: create nested classes for Order

---
+++ src/oop_111152_nadine/week08/OrderModels.kt
@@ -0,0 +1,9 @@
+package oop_111152_nadine.week08
+
+class City(val name: String)
+class Address(val city: City?)
+class DeliveryDetails(val address: Address?)
+class Order(
+    val deliveryDetails: DeliveryDetails?,
+    val totalPrice: Int?
+)
