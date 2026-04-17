b61c3f293eb0dec485afc5c5e25b5f762e7efb33
nadiinea <naadiinea@gmail.com>
2026-04-17 23:35:08 +0700

    week08: (task) implement payment processing with !!

--- src/oop_111152_nadine/week08/ApiParser.kt
+++ src/oop_111152_nadine/week08/ApiParser.kt
@@ -22,4 +22,15 @@ class ApiParser {
             else -> null
         }
     }
+
+    fun checkout(product: Product) {
+        val productId = when (product) {
+            is Electronic -> product.id
+            is Clothing -> product.id
+        }
+
+        val transactionId = JavaPaymentService.processPayment(productId)!!
+
+        println("Transaksi berhasil: $transactionId")
+    }
 }
