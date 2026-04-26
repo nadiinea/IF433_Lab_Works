f1b463cc1436ffb4478e9daab71fcb0aa4f8d568
nadiinea <naadiinea@gmail.com>
2026-04-26 21:04:57 +0700

    week09: create Student data class and list

---
+++ src/oop_111152_nadine/week09/ObjectChaining.kt
@@ -0,0 +1,11 @@
+package oop_111152_nadine.week09
+
+data class Student(val name: String, val gpa: Double)
+fun main() {
+    val students = listOf(
+        Student("Andi", 3.2),
+        Student("Budi", 3.9),
+        Student("Citra", 2.8),
+        Student("Dewi", 3.7)
+    )
+}
