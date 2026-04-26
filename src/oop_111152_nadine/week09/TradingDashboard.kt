725df1171601d3247a13dd44ad39fb5b349be4cc
nadiinea <naadiinea@gmail.com>
2026-04-26 21:08:26 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_111152_nadine/week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_111152_nadine.week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("XRPUSDT", "LONG", 5, 8.1, "OPEN"),
+        TradeLog("BTCUSDT", "SHORT", 15, -2.3, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 25, 12.0, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 10, -1.5, "OPEN")
+    )
+}
