cf5101b0e0bd2ad8fb731cf0cfed2d7b1fc89e08
nadiinea <naadiinea@gmail.com>
2026-04-26 21:13:00 +0700

    week09: (task) extract unique trading pairs

--- src/oop_111152_nadine/week09/TradingDashboard.kt
+++ src/oop_111152_nadine/week09/TradingDashboard.kt
@@ -20,4 +20,9 @@ fun main() {
     val worstPerformersString = losingTrades
         .sortedBy { it.roe }
         .map { "LOSS [${it.pair} - ${it.position}] : ${it.roe}% ROE (Lev: ${it.leverage}x)" }
+
+    val uniquePairs = tradeHistory
+        .map { it.pair }
+        .distinct()
+        .sorted()
 }
