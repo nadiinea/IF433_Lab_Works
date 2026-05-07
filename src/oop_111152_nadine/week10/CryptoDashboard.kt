39ace64bb50973a4038ee2485e11a1a2fbc352c0
nadiinea <naadiinea@gmail.com>
2026-05-08 00:05:57 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_111152_nadine/week10/CryptoDashboard.kt
+++ src/oop_111152_nadine/week10/CryptoDashboard.kt
@@ -6,4 +6,9 @@ fun main() {
     coinRepo.add(Coin("BTC", 1.5))
     coinRepo.add(Coin("ETH", 10.0))
     coinRepo.add(Coin("USDT", 5000.0))
+
+    val response = ApiResponse(
+        "200 OK",
+        coinRepo.getAll()
+    )
 }
