ae61d02015800d61548d0a849fa03be25e7fe282
nadiinea <naadiinea@gmail.com>
2026-05-08 08:54:55 +0700

    week11: implement with function for multiple operations

--- src/oop_111152_nadine/week11/ScopeConfiguration.kt
+++ src/oop_111152_nadine/week11/ScopeConfiguration.kt
@@ -16,4 +16,9 @@ fun main() {
         println("Log Sebelum ditambah: $it")
     }3.add(4)
     println("Setelah ditambah: $numbers")
+
+    println("\n === TEST WITH === ")
+    with(user) {
+        println("User Detail -> Nama: $name, Umur: $age")
+    }
 }
