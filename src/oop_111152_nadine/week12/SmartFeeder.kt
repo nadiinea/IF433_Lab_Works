e0416e9c03b41d746929ba3523ce3e1a203103a0
nadiinea <naadiinea@gmail.com>
2026-05-15 08:57:42 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_111152_nadine/week12/SmartFeeder.kt
@@ -0,0 +1,13 @@
+package oop_111152_nadine.week12
+
+fun dispenseKibble(
+    requestedGram: Int,
+    availableGram: Int,
+    isJammed: Boolean
+): Int {
+
+    require(requestedGram > 0) {
+        "Porsi kibble harus lebih dari 0 gr"
+    }
+    return availableGram
+}
