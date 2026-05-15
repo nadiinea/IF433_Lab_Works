3ff9daf725e1d40116a4514d05982d6d22b879dd
nadiinea <naadiinea@gmail.com>
2026-05-15 08:52:28 +0700

    week12: implement basic runCatching

---
+++ src/oop_111152_nadine/week12/RunCatchingExample.kt
@@ -0,0 +1,9 @@
+package oop_111152_nadine.week12
+
+fun main() {
+    println("=== TEST RUNCATCHING ===")
+    val result: Result<Int> = runCatching {
+        "42X".toInt()
+    }
+    println(result)
+}
