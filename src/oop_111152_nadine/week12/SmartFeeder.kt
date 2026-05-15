13ee9f29ba0a2f384a4f87af8f35051e0cd15d17
nadiinea <naadiinea@gmail.com>
2026-05-15 09:06:44 +0700

    week12: (task) execute feeding using runCatching

--- src/oop_111152_nadine/week12/SmartFeeder.kt
+++ src/oop_111152_nadine/week12/SmartFeeder.kt
@@ -40,4 +40,12 @@ fun main() {
     finally {
         println("Siklus pengecekan dispenser pagi selesai.")
     }
+
+    runCatching {
+        dispenseKibble(
+            requestedGram = 30,
+            availableGram = 1000,
+            isJammed = false
+        )
+    }
 }
