e4284bafaac3dfbf2f77a8f774c4696e4d4a45e1
nadiinea <naadiinea@gmail.com>
2026-05-22 23:36:36 +0700

    week13: (task) implement loadTrades with FileNotFoundException handling

--- src/oop_111152_nadine/week13/CryptoTradeLogger.kt
+++ src/oop_111152_nadine/week13/CryptoTradeLogger.kt
@@ -1,5 +1,6 @@
 package oop_111152_nadine.week13
 import java.io.File
+import java.io.FileNotFoundException
 
 data class TradeRecord(
     val id: Int,
@@ -35,5 +36,13 @@ fun saveTrades(trades: List<TradeRecord>, path: String) {
         trades.forEach {
             writer.println(it.toCsv())
         }
+    }
+}
+
+fun loadTrades(path: String): List<TradeRecord> {
+    return try {
+        File(path).readLines().mapNotNull { fromCsvTrade(it) }
+    } catch (e: FileNotFoundException) {
+        emptyList()
     }
 }
