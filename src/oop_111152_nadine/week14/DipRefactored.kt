d4608d70f36eadd6cdac527253bd535e8cdb19c3
nadiinea <naadiinea@gmail.com>
2026-05-29 09:04:16 +0700

    week14: create Database abstraction and implementations

---
+++ src/oop_111152_nadine/week14/DipRefactored.kt
@@ -0,0 +1,8 @@
+package oop_111152_nadine.week14
+
+interface Database {
+    fun query(sql: String): List<String>
+}
+class PostgresDatabase : Database {
+    override fun query(sql: String) = listOf("pg_data1", "pg_data2")
+}
