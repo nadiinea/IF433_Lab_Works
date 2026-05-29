fba36a5aaf02e7f70332f9ad21bad5ef571eaa9a
nadiinea <naadiinea@gmail.com>
2026-05-29 09:13:49 +0700

    week14: (task) implement PricingStrategy interface for OCP

--- src/oop_111152_nadine/week14/EcommerceRefactored.kt
+++ src/oop_111152_nadine/week14/EcommerceRefactored.kt
@@ -19,15 +19,36 @@ interface NotificationService {
     fun sendNotification(order: Order)
 }
 
-// ===== IMPLEMENTASI: EmailNotifier =====
 class EmailNotifier : NotificationService {
     override fun sendNotification(order: Order) {
         println("Email terkirim: Pesanan ${order.itemName} Anda telah dikonfirmasi!")
     }
 }
 
-// ===== SAFE ORDER PROCESSOR (inject lewat constructor) =====
 class SafeOrderProcessor(
     val repo: OrderRepository,
     val notifier: NotificationService
 )
+
+// ===== INTERFACE: PricingStrategy =====
+interface PricingStrategy {
+    fun calculate(price: Double): Double
+}
+
+// ===== IMPLEMENTASI =====
+class RegularPricing : PricingStrategy {
+    override fun calculate(price: Double): Double = price
+}
+
+class VipPricing : PricingStrategy {
+    override fun calculate(price: Double): Double = price * 0.90 // Diskon 10%
+}
+
+fun SafeOrderProcessor.processOrder(itemName: String, basePrice: Double, pricing: PricingStrategy) {
+    val finalPrice = pricing.calculate(basePrice)
+    println("Memproses pesanan $itemName seharga $finalPrice")
+
+    val order = Order(itemName, finalPrice, pricing::class.simpleName ?: "Unknown")
+    repo.saveOrder(order)
+    notifier.sendNotification(order)
+}
